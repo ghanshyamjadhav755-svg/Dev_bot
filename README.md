6f457a4f7d3977e7e86675a3a1bddf0561f5eade
ghanshyamjadhav755-svg <ghanshyamjadhav755@gmail.com>
2025-12-03 23:50:09 +0530

    Initial commit: dev_ws

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-## Robot Package Template
+## to launch gazebo with custom world
+ros2 launch my_bot launch_sim.launch.py world:=/home/jd/dev_ws/src/my_bot/worlds/maze.world 
 
-This is a GitHub template. You can make your own copy by clicking the green "Use this template" button.
+## launch rviz2
+ros2 run rviz2 -d src/my_bot/config/view_bot.rviz
 
-It is recommended that you keep the repo/package name the same, but if you do change it, ensure you do a "Find all" using your IDE (or the built-in GitHub IDE by hitting the `.` key) and rename all instances of `my_bot` to whatever your project's name is.
+## robot state publisher
+ros2 launch my_bot rs.launch.py
 
-Note that each directory currently has at least one file in it to ensure that git tracks the files (and, consequently, that a fresh clone has direcctories present for CMake to find). These example files can be removed if required (and the directories can be removed if `CMakeLists.txt` is adjusted accordingly).
+## mapping with slam_toolbox
+ros2 launch slam_toolbox online_async_launch.py params_file:=./src/my_bot/config/mapper_params_online_async.yaml use_sim_time:=true
+
---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,66 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    package_name = 'my_bot'
+
+    # ----------------------------------------
+    # Robot State Publisher
+    # ----------------------------------------
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory(package_name),
+                'launch',
+                'rsp.launch.py'
+            )
+        ]),
+        launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # ----------------------------------------
+    # Gazebo Server + Client
+    # ----------------------------------------
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('gazebo_ros'),
+                'launch',
+                'gazebo.launch.py'
+            )
+        ])
+    )
+
+    # ----------------------------------------
+    # Spawn Robot Entity
+    # ----------------------------------------
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-topic', 'robot_description',
+            '-entity', 'my_bot',
+            '-x', '0.5',
+            '-y', '0.5',
+            '-z', '0.05'
+        ],
+        output='screen'
+    )
+
+
+    # ----------------------------------------
+    # Launch Description
+    # ----------------------------------------
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+    ])
+
